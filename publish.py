a90648db5c1a48f54f88e71c486fb8f25bbae8fa
YashPradhan77 <83752766+YashPradhan77@users.noreply.github.com>
2024-05-23 14:40:46 +0530

    Update publish.py

--- publish.py
+++ publish.py
@@ -3,24 +3,10 @@ import redis
 # Connect to ElastiCache Redis
 client = redis.StrictRedis(host='rediscluster.eulx8y.ng.0001.use1.cache.amazonaws.com', port=6379, db=0)
 
-# Publish a message
-# client.publish('my_channel', 'Hello, Redis Pub/Sub!')
-    
+# Publish a message  
 def publish_message(channel, message):
     client.publish(channel, message)
     print(f"Published message: {message} to channel: {channel}")
 
-if __name__ == "__main__":
-    channel = 'my_channel'
-    message = 'Hello, Redis Pub/Sub!'
-    publish_message(channel, message)
+publish_message("my_channel","Hello")
 
-
-# Subscribe to a channel
-# pubsub = client.pubsub()
-# pubsub.subscribe('my_channel')
-
-# # Listen for messages
-# for message in pubsub.listen():
-#     if message['type'] == 'message':
-#         print(f"Received message: {message['data'].decode('utf-8')}")
